d68ceb34bb1c9933df7a11d6fa66f0abe07f74a6
Emiliya <vuntsova@aol.com>
2019-11-06 11:59:40 -0500

    Create beforeCallback.js

---
+++ beforeCallback.js
@@ -0,0 +1,36 @@
+const posts = [
+  {
+    title: 'Title 1 ',
+    body: ''
+  },
+  {
+    title: 'Title 2 ',
+    body: ''
+  }
+];
+
+const getPost = () => {
+  setTimeout(() => {
+    let content = '';
+
+    posts.forEach((post, index) => {
+      content += post.title;
+    });
+    console.log(content);
+  }, 2000);
+};
+
+const createPost = (yourNewPostTitle, yourNewPostBody) => {
+  setTimeout(() => {
+    const newPost = {
+      title: yourNewPostTitle
+    };
+    posts.push(newPost);
+  }, 1000);
+};
+createPost('Title 3 ');
+createPost('Title 4 ');
+createPost('Title 5 ');
+createPost('Title 6 ');
+createPost('Title 7 ');
+getPost();
